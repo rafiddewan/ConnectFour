0bf9a82ddeca4064be4347f4b176ab99deee62bb
Rafid Dewan <32348632+rafiddewan@users.noreply.github.com>
2018-12-05 23:47:10 -0500

    Update ConnectButton.java

--- ConnectButton.java
+++ ConnectButton.java
@@ -2,27 +2,49 @@ package sample;
 
 import javafx.scene.control.Button;
 
+/**
+ * ConnectButton
+ * @author Rafid Dewan
+ */
 public class ConnectButton extends Button {
     private int row;
     private int column;
 
+    /**
+     * Constructor for the button
+     * @param label
+     * @param row
+     * @param column
+     */
     public ConnectButton(String label, int row, int column)
     {
+        super(label);
         this.row = row;
         this.column = column;
-        this.setText(label);
     }
 
+    /**
+     * Returns the row
+     * @return int
+     */
     public int getRow()
     {
         return row;
     }
 
+    /**
+     * Returns the column
+     * @return int
+     */
     public int getColumn()
     {
         return column;
     }
 
+    /**
+     * Returns a string
+     * @return String
+     */
     @Override
     public String toString()
     {
